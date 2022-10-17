70b7067c0475f3806ddb1a3faddc2a9e753c615b
vverac <viverace@gmail.com>
2022-10-17 16:35:14 -0300

    add fuction  cargarUsuarios

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+$(document).ready(function () {
+  cargarUsuarios();
+});
+
 const submit = document.querySelector('#submit');
 
 const savePaciente = async () => {
@@ -8,9 +12,6 @@ const savePaciente = async () => {
   let direccion = document.getElementById("direccion").value
   let email = document.getElementById("email").value
   document.getElementById("myForm").reset();
-
-  let response = await fetch('http://localhost:8080/paciente/getPacientes');
-  console.log(response);
 
   let response2 = await fetch('http://localhost:8080/paciente/save', {
     method: 'POST',
@@ -24,17 +25,49 @@ const savePaciente = async () => {
       telefono: telefono,
       direccion: direccion,
       email: email
-
     })
   })
   console.log(response2);
+}
+submit.addEventListener('click', savePaciente);
 
-  // $('#exampleModalCenter').modal('toggle'); //or  $('#IDModal').modal('hide');
 
-  // $('#exampleModal').modal('toggle'); //or  $('#IDModal').modal('hide');
 
-  // return false;
+async function cargarUsuarios() {
+  const request = await fetch('http://localhost:8080/paciente/getPacientes', {
+    method: "GET",
+    header: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
+  })
+  const usuarios = await request.json();
 
+  let listadoHtml = ''
+  for (let usuario of usuarios) {
+    // let botonEliminar = "<a href=\"#\" onclick='eliminarUsuario(" + usuario.id + ")' class=\"btn btn-danger btn-circle btn-sm\"><i class=\"fas fa-trash\"></i></a>";
+
+
+    let usuarioHtml = '  <tr><td>' + usuario.nombre + '</td><td>' + usuario.rut + '</td><td>'
+      + usuario.direccion + '</td><td>' + usuario.telefono + '</td><td>' + usuario.email + '</td></tr>'
+
+    listadoHtml += usuarioHtml
+  }
+  document.querySelector("#usuarios tbody").outerHTML = listadoHtml;
 }
 
-submit.addEventListener('click', savePaciente);
+
+// async function eliminarUsuario(id) {
+//   if (!confirm("¿Desea eliminar este usuario?")) {
+//     return;
+//   }
+//   const request = await fetch('http://localhost:8080/paciente/getPacientes' + id, {
+//     method: "DELETE",
+//     //indica que el contenido es json
+//     header: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json',
+//     }
+//   })
+//   location.reload();
+// }
